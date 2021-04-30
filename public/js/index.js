a66a82cf23561c865da83958ee0ce26beeb03cd7
Brayhan De Aza <brayhandeaza@gmail.com>
2021-04-30 15:00:16 -0400

    tokenomics

--- public/js/index.js
+++ public/js/index.js
@@ -5,13 +5,11 @@ var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
         labels: [
-            'Red',
-            'Yellow',
-            'Blue'
+            
         ],
         datasets: [{
             // label: '# of Votes',
-            data: [20,30,50],
+            data: [40,20,40],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -23,6 +21,11 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 206, 86, 1)'
             ],
             borderWidth: 1,
-        }]
+        }],
+        
+    }, options: {
+        title: {
+            display : false
+        }
     }
 })
